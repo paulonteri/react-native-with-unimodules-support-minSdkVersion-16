6d5b68cbb4cde4cf86a5917b208999f13d6bb7f7
Paul Onteri <paul@inuua.net>
2021-03-04 14:17:31 +0300

    add unimodules

--- android/app/src/main/java/com/reactnativewithunimodules/MainApplication.java
+++ android/app/src/main/java/com/reactnativewithunimodules/MainApplication.java
@@ -1,5 +1,7 @@
 package com.reactnativewithunimodules;
 
+import com.reactnativewithunimodules.generated.BasePackageList; // https://docs.expo.io/bare/installing-unimodules/
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -10,8 +12,17 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+// https://docs.expo.io/bare/installing-unimodules/
+import java.util.Arrays;
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
+//
 
 public class MainApplication extends Application implements ReactApplication {
+
+  // https://docs.expo.io/bare/installing-unimodules/
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -26,6 +37,15 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          // https://docs.expo.io/bare/installing-unimodules/
+          // Add unimodules
+          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+            new ModuleRegistryAdapter(mModuleRegistryProvider)
+          );
+          packages.addAll(unimodules);
+          //
+
           return packages;
         }
 
